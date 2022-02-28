7ebea67b931ad90f5b4c2725ec75923513009080
joaovictor3g <jvdias79797@gmail.com>
2022-02-28 19:51:57 -0300

    feat: ending "Redux na pratica"

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,37 +1,21 @@
-import { useCallback, useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 import { api } from "../services/api";
-import { addProductToCart } from "../store/modules/cart/actions";
 import { IProduct } from "../store/modules/cart/types";
+import { CatalogItem } from "./CatalogItem";
 
 export function Catalog() {
-  const dispatch = useDispatch();
   const [catalog, setCatalog] = useState<IProduct[]>([]);
 
   useEffect(() => {
     api.get("products").then((response) => setCatalog(response.data));
   }, []);
 
-  const handleAddProductToCart = useCallback(
-    (product: IProduct) => {
-      dispatch(addProductToCart(product));
-    },
-    [dispatch]
-  );
-
   return (
     <main>
       <h1>Catalog</h1>
 
       {catalog.map((product) => (
-        <article key={product.id}>
-          <strong>{product.title}</strong>
-          {" - "}
-          <span>{product.price}</span>{" "}
-          <button type="button" onClick={() => handleAddProductToCart(product)}>
-            Comprar
-          </button>
-        </article>
+        <CatalogItem key={product.id} product={product} />
       ))}
     </main>
   );
---
+++ src/components/CatalogItem.tsx
@@ -0,0 +1,27 @@
+import { useCallback } from "react";
+import { useDispatch } from "react-redux";
+import { addProductToCart } from "../store/modules/cart/actions";
+import { IProduct } from "../store/modules/cart/types";
+
+interface CatalogItemProps {
+  product: IProduct;
+}
+
+export function CatalogItem({ product }: CatalogItemProps) {
+  const dispatch = useDispatch();
+
+  const handleAddProductToCart = useCallback(() => {
+    dispatch(addProductToCart(product));
+  }, [dispatch, product]);
+
+  return (
+    <article key={product.id}>
+      <strong>{product.title}</strong>
+      {" - "}
+      <span>{product.price}</span>{" "}
+      <button type="button" onClick={handleAddProductToCart}>
+        Comprar
+      </button>
+    </article>
+  );
+}
